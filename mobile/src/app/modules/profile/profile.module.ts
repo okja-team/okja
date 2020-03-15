f1ea0006fe7825ffc7413b5abcee4ca12174d8dd
Vittorio Lo Preiato <vittorio.lo.preiato@avanade.com>
2020-03-15 16:23:24 +0100

    [Profile] - added reactiveForm

--- mobile/src/app/modules/profile/profile.module.ts
+++ mobile/src/app/modules/profile/profile.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { ProfilePageRoutingModule } from './profile-routing.module';
 
 import { ProfilePage } from './profile.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    ProfilePageRoutingModule
+    ProfilePageRoutingModule,
+    TranslateModule.forChild()
   ],
   declarations: [ProfilePage]
 })
