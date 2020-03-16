d476f2626cafb8bcffe0f9832aa02cafe3788375
Vittorio Lo Preiato <vittorio.lo.preiato@avanade.com>
2020-03-16 23:23:48 +0100

    [project] - added ngx-take-until-destroy, added baseUrl

--- mobile/src/app/app.module.ts
+++ mobile/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { NgModule } from '@angular/core';
