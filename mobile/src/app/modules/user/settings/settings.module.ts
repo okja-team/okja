f422039c097ca379a29e3a6605f28569d73f847b
calca <gianluigi.calcaterra@gmail.com>
2020-03-29 12:21:51 +0200

    Rename to Settings

--- mobile/src/app/modules/user/settings/settings.module.ts
+++ mobile/src/app/modules/user/settings/settings.module.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab3Page } from './tab3.page';
+import { SettingsPage } from './settings.page';
 import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
@@ -11,9 +11,9 @@ import { TranslateModule } from '@ngx-translate/core';
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab3Page }]),
+    RouterModule.forChild([{ path: '', component: SettingsPage }]),
     TranslateModule.forChild()
   ],
-  declarations: [Tab3Page]
+  declarations: [SettingsPage]
 })
 export class Tab3PageModule {}
--- mobile/src/app/modules/user/settings/settings.page.spec.ts
+++ mobile/src/app/modules/user/settings/settings.page.spec.ts
@@ -2,19 +2,19 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-import { Tab3Page } from './tab3.page';
+import { SettingsPage } from './settings.page';
 
 describe('Tab3Page', () => {
-  let component: Tab3Page;
-  let fixture: ComponentFixture<Tab3Page>;
+  let component: SettingsPage;
+  let fixture: ComponentFixture<SettingsPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab3Page],
+      declarations: [SettingsPage],
       imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Tab3Page);
+    fixture = TestBed.createComponent(SettingsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
