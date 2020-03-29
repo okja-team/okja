fb55528982cbc28136647e850d18787d291db397
Domenico Citera <citerad@users.noreply.github.com>
2020-03-29 21:05:16 +0200

    fix main icon

--- mobile/src/app/modules/tabs/tabs.page.ts
+++ mobile/src/app/modules/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(private readonly router: Router) {}
+
+  async onMainClick() {
+    await this.router.navigate(['home/tabs/map']);
+  }
 
 }
