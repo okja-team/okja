26312c968b9e9e18f057f1c64d002982f4f5408a
calca <gianluigi.calcaterra@gmail.com>
2020-03-29 12:29:12 +0200

    TestBed

--- mobile/src/app/modules/user/settings/settings.page.spec.ts
+++ mobile/src/app/modules/user/settings/settings.page.spec.ts
@@ -1,17 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { SettingsPage } from './settings.page';
 
-describe('Tab3Page', () => {
+describe('SettingsPage', () => {
   let component: SettingsPage;
   let fixture: ComponentFixture<SettingsPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [SettingsPage],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+      imports: [IonicModule.forRoot()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SettingsPage);
