532609fc530c17657701ffee40aa59f00a76c810
yuriy <sllouyssgort@gmail.com>
2020-04-18 11:30:16 +0800

    store selected language in local storage

--- mobile/src/app/services/translate-config.service.ts
+++ mobile/src/app/services/translate-config.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Plugins } from '@capacitor/core';
+
+const { Storage } = Plugins;
 
 @Injectable({
   providedIn: 'root'
@@ -12,18 +15,34 @@ export class TranslateConfigService {
     private translate: TranslateService
   ) { }
 
-  getDefaultLanguage(){
-    const browserLanguage = this.translate.getBrowserLang();
-    const language = this.supportedLanguages.indexOf(browserLanguage) >= 0 ? browserLanguage : 'en'
+  async getDefaultLanguage() {
+    let language  = await this.getStoredLanguage()
+    if (!language) {
+      const browserLanguage = this.translate.getBrowserLang();
+      language = this.supportedLanguages.indexOf(browserLanguage) >= 0 ? browserLanguage : 'en'
+    }
     this.translate.setDefaultLang(language);
     return language;
   }
 
-  setLanguage(setLang) {
+  setLanguage(setLang: string) {
     this.translate.use(setLang);
+    // noinspection JSIgnoredPromiseFromCall
+    this.storeLanguage(setLang)
   }
 
   translateInstant(key: string) : string{
     return this.translate.instant(key);
+  }
+
+  async getStoredLanguage() {
+    return (await Storage.get({ key: 'TranslateConfigService.language' }))?.value
+  }
+
+  async storeLanguage(language: string) {
+    await Storage.set({
+      key: 'TranslateConfigService.language',
+      value: language
+    });
   }
 }
